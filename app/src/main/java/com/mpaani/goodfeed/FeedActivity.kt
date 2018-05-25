9b005a86d5df609a9cdca2eedd2b3bc5cfc80c00
Calvin Noronha <calvin.nrnha@gmail.com>
2018-05-25 20:57:38 +0530

    Created stub Android Project

---
+++ app/src/main/java/com/mpaani/goodfeed/FeedActivity.kt
@@ -0,0 +1,12 @@
+package com.mpaani.goodfeed
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
